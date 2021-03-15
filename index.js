b726176645ef4283266dee325aeae125e563d567
Cameron Harvey <charvey0@gmail.com>
2021-03-14 22:44:33 -0700

    wrote the main menu and the create branch of the cli

--- index.js
+++ index.js
@@ -1,25 +1,184 @@
-// require node, inquirer, mysql
-// make schema
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '709Vergo!',
+  database: 'employee_db',
+});
+
+const action = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['[CREATE]', '[UPDATE]', '[VIEW]','[DELETE]', '[EXIT]'],
+    })
+    .then((ans) => {
+        switch (ans.action) {
+            case '[CREATE]':
+                console.log("create called");
+                create();
+                break;
+            case '[UPDATE]':
+//                update();
+                break;
+            case '[VIEW]':
+//                view();
+                break;
+            case '[DELETE]':
+//                del();
+                break;
+            default:
+                connection.end();
+        }
+    });
+};
 
 
-// employee
-// id int AI PK
-// first_name varchar(30)
-// last_name varchar(30)
-// role_id int FK
-// manager_id int FK
+const create = () => {
+    inquirer
+    .prompt({
+      name: 'create',
+      type: 'list',
+      message: 'What would you like to create?',
+      choices: ['Create a new employee', 'Create a new role', 'Create a new department', 'Go back to the main menu'],
+    })
+    .then((ans) => {
+        switch (ans.create) {
+            case 'Create a new employee':
+                createEmployee();
+                break;
+            case 'Create a new role':
+                createRole();
+                break;
+            case 'Create a new department':
+                createDepartment();
+                break;
+            default:
+                action();
+        }
+    });
+};
+
+const createEmployee = () => {
+    inquirer
+    .prompt([
+      {
+        name: 'first_name',
+        type: 'input',
+        message: "What is the employee's first name?",
+      },
+      {
+        name: 'last_name',
+        type: 'input',
+        message: "What is the employee's first name?",
+      },
+      {
+        name: 'role_id',
+        type: 'input',
+        message: "What is the employee's role id?",
+      },
+      {
+        name: 'manager_id',
+        type: 'input',
+        message: "What is the employee's manager's id?",
+      }
+    ])
+    .then((answer) => {
+      connection.query(
+        'INSERT INTO Employee SET ?',
+        {
+          first_name: answer.first_name,
+          last_name: answer.last_name,
+          role_id: answer.role_id || null,
+          manager_id: answer.manager_id || null,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log('Your employee was successfully added.');
+          action();
+        }
+      );
+    });
+};
 
 
-// role
-// id int AI PK
-// title varchar(30)
-// salary decimal
-// department_id int FK
+const createRole = () => {
+    inquirer
+    .prompt([
+      {
+        name: 'title',
+        type: 'input',
+        message: "What is the title of the new role?",
+      },
+      {
+        name: 'salary',
+        type: 'input',
+        message: "What is the salary?",
+      },
+      {
+        name: 'is_management',
+        type: 'list',
+        message: "Is this role a managing role? (1 for 'YES'; 0 for 'NO'",
+        choices: [1, 0],
+      },
+      {
+        name: 'department_id',
+        type: 'input',
+        message: "What is the id of the department this role is under?",
+      },
+
+    ])
+    .then((answer) => {
+      connection.query(
+        'INSERT INTO role SET ?',
+        {
+          title: answer.title,
+          salary: answer.salary,
+          is_management: answer.is_management,
+          department_id: answer.department_id
+        },
+        (err) => {
+          if (err) throw err;
+          console.log('The new role was successfully added.');
+          action();
+        }
+      );
+    });
+};
 
 
-// department
-// id int AI PK
-// name varchar(30)
+const createDepartment = () => {
+    inquirer
+    .prompt([
+      {
+        name: 'name',
+        type: 'input',
+        message: "What is the name of the new department?",
+      }
+    ])
+    .then((answer) => {
+      connection.query(
+        'INSERT INTO department SET ?',
+        {
+          name: answer.name
+        },
+        (err) => {
+          if (err) throw err;
+          console.log('The new department was successfully added.');
+          action();
+        }
+      );
+    });
+};
 
 
-// make seeds
+
+connection.connect((err) => {
+  if (err) throw err;
+  action();
+});
